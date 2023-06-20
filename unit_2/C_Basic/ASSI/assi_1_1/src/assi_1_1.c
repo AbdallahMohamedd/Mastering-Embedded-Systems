d933bb00c6a47c0ce53ce6591db7119058e1fb26
Abdullah Mohamed <abdallahmohamed4197@gmail.com>
2023-06-20 12:28:46 +0300

    add assi_1_1

---
+++ unit_2/C_Basic/ASSI/assi_1_1/src/assi_1_1.c
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+ Name        : assi_1_1.c
+ Author      : Abdullah  Mohamed
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+int main ()
+{
+	printf("C Programming");
+}
