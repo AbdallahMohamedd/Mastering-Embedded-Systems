9bcf18a9992e65a4fb2040de138889557a9d50a5
Abdullah Mohamed <abdallahmohamed4197@gmail.com>
2023-06-26 22:45:20 +0300

    third assignment

---
+++ unit_2/Array_and_String/ASSI/TRANSPOSE_MATRIX_3/src/TRANSPOSE_MATRIX_3.c
@@ -0,0 +1,55 @@
+/*
+ ============================================================================
+ Name        : transpose_matrix.c
+ Author      : Abdullah Mohamed
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int rows, columns;
+	printf("Enter the number of rows: ");
+	fflush(stdin);  fflush(stdout);
+	scanf("%d",&rows);
+	printf("Enter the number of columns: ");
+	fflush(stdin);  fflush(stdout);
+	scanf("%d",&columns);
+	int matrix[rows][columns];
+	////////////get the matrix///////////
+	printf("Enter the elementS\n");
+	for(int i=0;i<rows;i++)
+	{
+		for(int k=0;k<columns;k++)
+		{
+			printf("Enter the element a%d%d: ",i,k);
+			fflush(stdin);  fflush(stdout);
+			scanf("%d",&matrix[i][k]);
+		}
+	}
+	//////////print the matrix/////////
+	printf("the matrix is \n");
+	for(int i=0;i<rows;i++)
+	{
+		for(int k=0;k<columns;k++)
+		{
+			printf("%d ",matrix[i][k]);
+		}
+		printf("\n");
+	}
+	//////////transpose the matrix/////////
+	printf("transpose of matrix is \n");
+	for(int i=0;i<columns;i++)
+	{
+		for(int k=0;k<rows;k++)
+		{
+			printf("%d ",matrix[k][i]);
+		}
+		printf("\n");
+	}
+	////////////////end//////////////////
+}
