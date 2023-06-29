e10002993bf900ec93bb1d1c7666a3856e746ccb
Abdullah Mohamed <abdallahmohamed4197@gmail.com>
2023-06-30 02:10:34 +0300

    lab 2 find min. no. of data

---
+++ unit_2/C_Function/LAB/lab_2_find_min_value_of_array/find_min_value.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int minno (int array[],int n);
+int main()
+{
+    int n, data[50];
+    printf("Enter number of element: ");
+    scanf("%d", &n);
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++)  scanf("%d", &data[i]);
+    printf("the data is ");
+    for (int i = 0; i < n; i++)  printf("%d ", data[i]);
+    printf("\n");
+    //////////////////////////////////////////////////////////////
+    printf("the min. no. in data is %d",minno (data,n));
+}
+
+int minno (int array[],int n)
+{
+    int min=0;
+    for(int i=0; i<n; i++)
+    {
+        if(array[i]<min)
+        {
+            min=array[i];
+        }
+    }
+    return min;
+}
