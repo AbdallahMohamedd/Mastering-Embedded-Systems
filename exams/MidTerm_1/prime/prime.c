22ddc1a2b114fb52ba7e83b0c78983735e2f5e35
Abdullah Mohamed <abdallahmohamed4197@gmail.com>
2023-07-04 00:49:49 +0300

    Q2 in midterm_1

---
+++ exams/MidTerm_1/prime/prime.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+int prime(int n);
+int main()
+{
+    unsigned int start, end;
+    printf("Enter two numbers(intervals):\n");
+    scanf("%d %d", &start, &end);
+    //////////////////////////////////////////
+    printf("The prime number between %d and %d are ", start, end);
+    for (int i = start; i <= end; i++)
+    {
+        if (!prime(i))
+            continue;
+        else
+            printf("%d ", prime(i));
+    }
+}
+int prime(int n)
+{
+    for (int c = 2; c < n; c++)
+        if ((n % c) == 0)
+            return 0;
+    return n;
+}
